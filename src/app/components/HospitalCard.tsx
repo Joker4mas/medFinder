54b4409ee740577c1200e3a4c192a450da780fcc
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-08-17 14:14:04 +0100

    fixed image issue

--- src/app/components/HospitalCard.tsx
+++ src/app/components/HospitalCard.tsx
@@ -1,6 +1,6 @@
 // import Image from 'next-/Image';
 import { useState } from "react";
-import Image from "next/Image";
+import Image from "next/image";
 import GoogleImage from "../images/google-maps.svg";
 
 interface HospitalCardProps {
