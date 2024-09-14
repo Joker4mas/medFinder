3b1d42d759080aa5e0cbfdf72c997833e94941ca
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-09-14 14:13:23 +0100

    a little tweaking

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,17 +7,17 @@ export default function About() {
         <Accordion>
           <AccordionItem
             header="Why MedFinder?"
-            className="w-h-screen items-center p-4 mb-4 text-center"
+            className="items-center p-4 mb-4 text-center"
           >
             <div className="p-4">
-              <p className="">
+              <p className=" w-full p-2 md:w-4/5 mx-auto text-lg">
                 In todays fast-paced world, finding the right medical facility
                 quickly and efficiently is crucial. MedFinder simplifies this
                 process by offering a comprehensive, user-friendly platform that
                 connects you with a wide range of healthcare providers in your
                 area.
               </p>
-              <span className="my-2">
+              <span className="my-2 md:w-2/5 text-lg">
                 How to Use MedFinder Visit Our Website Go to MedFinder.com and
                 click on the Find Medical Facilities button.
               </span>
@@ -46,7 +46,7 @@ export default function About() {
             className="w-h-screen items-center p-4 mb-4 text-center"
           >
             <h3 className="font-bold my-2"> Search</h3>
-            <p className="p-4">
+            <p className="p-4 w-full md:w-4/5 mx-auto">
               Our advanced search engine allows you to find hospitals, clinics,
               pharmacies, and specialized medical centers based on your location
               and specific needs. Simply enter your zip code or use your
@@ -60,7 +60,7 @@ export default function About() {
             className="w-h-screen items-center p-4 mb-4 text-center"
           >
             <h3 className="font-semibold text-center"> Search</h3>
-            <p className="p-4"> 
+            <p className="p-4 md:w-4/5 mx-auto"> 
               Our advanced search engine allows you to find hospitals, clinics,
               pharmacies, and specialized medical centers based on your location
               and specific needs. Simply enter your zip code or use your
@@ -74,7 +74,7 @@ export default function About() {
             className="w-h-screen items-center p-4 mb-4 text-center"
           >
             <h3 className="font-semibold "> Search</h3>
-            <p className="p-4">
+            <p className="p-4 md:w-4/5 mx-auto">
               Our advanced search engine allows you to find hospitals, clinics,
               pharmacies, and specialized medical centers based on your location
               and specific needs. Simply enter your zip code or use your
